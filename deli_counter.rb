00054ae1ca0807020c69ccde29586ac496b4f923
Andres Carvajal <jc1993box@hotmail.com>
2020-02-03 19:38:15 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -11,7 +11,7 @@ def line (deli)
 end
 
 
-  def take_a_number(deli, new_person)
+def take_a_number(deli, new_person)
  deli << new_person 
  puts "Welcome, #{new_person}. You are number #{deli.length} in line."
 end
