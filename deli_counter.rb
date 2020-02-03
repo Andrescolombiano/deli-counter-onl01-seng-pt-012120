a15ed812107451c445db2553c4faf594b731fb98
Andres Carvajal <jc1993box@hotmail.com>
2020-02-03 19:29:11 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,13 @@
-# Write your code here.
+def line (deli)
+ if deli.length == 0
+   puts "The line is currently empty."
+ else 
+ str = "The line is currently:"
+  deli.each.with_index(1) do |person, i|
+  str << " #{i}. #{person}"
+  end
+  puts str
+ end
+end
+
+def take_a_number (deli, string)
